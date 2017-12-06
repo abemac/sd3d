11a2261c2216a806d1240097d2c75558f09c455d
Abraham McIlvaine <abraham@the.mcilvaines.space>
2017-12-06 01:49:22 -0500

    welcome page complete

--- webserver/big-print/src/app/app.module.ts
+++ webserver/big-print/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import {MatCardModule,MatGridListModule} from '@angular/material'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
