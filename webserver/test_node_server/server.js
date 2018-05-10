e34feebf6f776d4cbe0666ad025ec919944a6df4
Abraham McIlvaine <abemac@udel.edu>
2018-05-10 11:15:10 -0400

    added node REST server

--- webserver/test_node_server/server.js
+++ webserver/test_node_server/server.js
@@ -1,4 +1,45 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
+const spawn = require("child_process").spawn;
+const url = require('url');
+
+app.use(cors())
 app.use(express.static('../big-print/dist/'))
+var running = false
+app.route('/startDemo').get((req,res)=>{
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    if(query.token == '6288260a217ca1838cd8c3f25d3fe475'){
+        console.log('authenticated')
+    }else{
+        res.status(500).send("ERROR")
+        return
+    }
+    if(running == false){
+        running=true
+        var pythonProcess = spawn('python3',["test.py"]);
+        pythonProcess.stdout.on('data', function (data){
+            console.log(String(data))
+            running=false
+        });
+        res.status(200).send("SUCCESS")
+    }else{
+        res.status(500).send("ERROR")
+    }
+    
+})
+app.route('/status').get((req,res)=>{
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    if(query.token == '6288260a217ca1838cd8c3f25d3fe475'){
+        console.log('authenticated')
+    }else{
+        res.status(500).send("ERROR")
+        return
+    }
+    res.status(200).send(running)
+    
+    
+})
 app.listen(3000,'0.0.0.0', () => console.log('Started on port 3000'))
---
+++ webserver/test_node_server/test.py
@@ -0,0 +1,5 @@
+import sys
+print ("Hello from python")
+import time
+time.sleep(2)
+sys.stdout.flush()
